28ff8c104c4cdedde7908a5bb55c51eb3c537b91
francizjamez <francizjamez@gmail.com>
2021-05-17 18:03:18 +0800

    bigger buttons

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,11 +1,12 @@
 import { useContext } from "react";
+import styled from "styled-components";
 
 import CounterStore from "../../contexts/CounterStore";
 
 const Index = () => {
   const { counterState, dispatch } = useContext(CounterStore);
   return (
-    <div>
+    <Counter>
       <h2>Counter</h2>
       <div className="flex">
         <button onClick={() => dispatch("decrement")}>-</button>
@@ -13,8 +14,23 @@ const Index = () => {
         <button onClick={() => dispatch("increment")}>+</button>
       </div>
       <button onClick={() => dispatch("reset")}>reset</button>
-    </div>
+    </Counter>
   );
 };
+
+const Counter = styled.div`
+  button {
+    padding: 0.5rem 1rem;
+    font-size: 1rem;
+    cursor: pointer;
+  }
+  span {
+    font-size: 2rem;
+  }
+
+  .flex {
+    align-items: center;
+  }
+`;
 
 export default Index;
